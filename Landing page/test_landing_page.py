1cbc91806ce4808151984af66267344bccebbd72
Kavindu <kmvithanage48@gmail.com>
2024-06-09 19:50:33 +0530

    updates

--- Landing page/test_landing_page.py
+++ Landing page/test_landing_page.py
@@ -1,22 +1,30 @@
 import pytest
 from selenium import webdriver
-import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import configuration.config
 
 
-class Test_landing_page:
-    driver = ''
+class TestLandingPage:
+    def setup_method(self):
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.implicitly_wait(10)
+        self.wait = WebDriverWait(self.driver, 10)
+        self.driver.get(configuration.config.BaseURL)
+        self.driver.maximize_window()
 
-    def setup(self):
-        # options = webdriver.ChromeOptions()
-        # options.add_experimental_option("detach", True)
-        self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(5)
-        # self.wait = WebDriverWait(self.driver, 10)
-        self.driver.get("https://uat.ushop.lk/")
+    def test_navigate_correct_page(self):
+        get_started_btn = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//button[normalize-space()='Get Started']")))
+        if get_started_btn:
+            print("Successfully loaded page")
 
-    def test_d(self):
-         
+    def 
+
+
+
+
+
+
